e1fec92b7e267e2fe83c05242ea118865aa3ace0
importOnurDogan <122278023+importOnurDogan@users.noreply.github.com>
2023-09-02 16:55:02 +0300

    Update 5
    More features

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
 user_db = {
-    'jack': {'username': 'jack', 'date_joined': '2021-12-01', 'location': 'New York', 'age': 28},
-    'jill': {'username': 'jill', 'date_joined': '2021-12-02', 'location': 'Los Angeles', 'age': 19},
+    'jack': {'username': 'jack', 'date_joined': '2021-12-01', 'location': 'New York', 'age': 28},'\n'
+    'jill': {'username': 'jill', 'date_joined': '2021-12-02', 'location': 'Los Angeles', 'age': 19},'\n'
     'jane': {'username': 'jane', 'date_joined': '2021-12-03', 'location': 'Toronto', 'age': 52}
 }
 
@@ -13,3 +13,12 @@ app = FastAPI()
 def get_users():
     user_list = list(user_db.values())
     return user_list
+
+@app.get('/users/{username}')
+def get_users_path(username: str):
+    return user_db[username]
+
+@app.get('/')
+def get_users_query(limit: int):
+    user_list = list(user_db.values())
+    return user_list[:limit]
