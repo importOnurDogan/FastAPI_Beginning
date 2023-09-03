5eb5704c8ef1e8993f368bdbb0b5697a8cbebf30
importOnurDogan <122278023+importOnurDogan@users.noreply.github.com>
2023-09-03 12:45:59 +0300

    Update 6
    Add user, request body with Pydantic model.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from pydantic  import BaseModel
 
 user_db = {
     'jack': {'username': 'jack', 'date_joined': '2021-12-01', 'location': 'New York', 'age': 28},'\n'
     'jill': {'username': 'jill', 'date_joined': '2021-12-02', 'location': 'Los Angeles', 'age': 19},'\n'
     'jane': {'username': 'jane', 'date_joined': '2021-12-03', 'location': 'Toronto', 'age': 52}
 }
+
+
+class User(BaseModel):
+    username: str = Field(min_length = 3, max_length = 24)
+    date_joined: date
+    lacation: Optional[str] = None
+    age: int = Field(None, gt=5, lt=79)
+
 
 app = FastAPI()
 
@@ -22,3 +31,9 @@ def get_users_path(username: str):
 def get_users_query(limit: int):
     user_list = list(user_db.values())
     return user_list[:limit]
+
+@app.post('user')
+def create_user(user: User):
+    username = user.username
+    user_db[username] = user.dict()
+    return {'message': f'Succesfully created user: {username}'}
