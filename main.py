4c0431b7344d15cf817796ed259d246da7a7d5d6
importOnurDogan <122278023+importOnurDogan@users.noreply.github.com>
2023-09-03 16:29:23 +0300

    Update7
    new features: delete, put, patch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from pydantic  import BaseModel
+from pydantic  import BaseModel, Field
+from typing import Optional
+from datetime import date
 
 user_db = {
     'jack': {'username': 'jack', 'date_joined': '2021-12-01', 'location': 'New York', 'age': 28},'\n'
@@ -7,33 +9,58 @@ user_db = {
     'jane': {'username': 'jane', 'date_joined': '2021-12-03', 'location': 'Toronto', 'age': 52}
 }
 
-
 class User(BaseModel):
-    username: str = Field(min_length = 3, max_length = 24)
+    username: str = Field(min_length=3, max_length=24)
     date_joined: date
     lacation: Optional[str] = None
-    age: int = Field(None, gt=5, lt=79)
+    age: int = Field(None, gt=5, lt=130)
 
 
 app = FastAPI()
 
-
+#Showing the users
 @app.get('/users')
-def get_users():
-    user_list = list(user_db.values())
-    return user_list
-
-@app.get('/users/{username}')
-def get_users_path(username: str):
-    return user_db[username]
-
-@app.get('/')
 def get_users_query(limit: int):
     user_list = list(user_db.values())
     return user_list[:limit]
 
-@app.post('user')
+
+# Showing the selected user
+@app.get('/users/{username}')
+def get_users_path(username: str):
+    if username not in user_db:
+        return False
+    return user_db[username]
+
+
+# Creating new user
+@app.post('/users')
 def create_user(user: User):
     username = user.username
+    if username in user_db:
+        return False
     user_db[username] = user.dict()
     return {'message': f'Succesfully created user: {username}'}
+
+
+# Deleting the user
+@app.delete('/users/{username}')
+def delete_user(username: str):
+    del user_db[username]
+    return {'message':f'Successfully deleted user:{username}'}
+
+
+# Update the User
+@app.put('/users')
+def update_user(user: User):
+    username = user.username
+    user_db[username] = user.dict()
+    return {'message':f'Successfully updated user:{username}'}
+
+
+# Update the user's selected attribute
+@app.patch('/users')
+def update_user_partial(user: User):
+    username = user.username
+    user_db[username].update(user.dict())
+    return {'message':f'Successfully updated user:{username}'}
